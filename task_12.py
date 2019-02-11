076d6e0e9c340f955274408d9447bf238ac34556
Leo Vergara <leovergarah@gmail.com>
2019-02-10 20:40:45 -0500

    (Completed): TASK-12 & Test is running successfully

---
+++ task_12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 12"""
+
+import decimal
+import fractions
+
+INTVAL = 1
+FLOATVAL = 0.1
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
