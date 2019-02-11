b6dca21139508b8dd22001862b5316a9d096c078
Leo Vergara <leovergarah@gmail.com>
2019-02-10 20:40:19 -0500

    (Completed): TASK-9 & Test is running successfully

--- task_09.py
+++ task_09.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""
+A computer that can build the question.
+
+Not just any question:
+No one really cares about how many Vogons
+would it take to change a lightbulb?
+
+We need a computer that can build the
+ULTIMATE QUESTION. This computer will be called DEEP THOUGHT
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
-
 INFINITE_IMPROBABILITY = 'browning motion'
